88449fbeac325f50b578febddbde15e79a720ad0
JustinHLott <justin.h.lott.75@gmail.com>
2025-01-23 09:29:21 -0700

    Update YAML script

--- MyCoreApp/obj/Release/net8.0/MyCoreApp.AssemblyInfo.cs
+++ MyCoreApp/obj/Release/net8.0/MyCoreApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyCoreApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d8bddfa69d312260c9c7f600dab9c0194b37b9d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a06956d0a0e07cb363218ea44bbd7c224c938410")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyCoreApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyCoreApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
